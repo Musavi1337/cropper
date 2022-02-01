428d634e4721f2faf19ba9f3de67c955c2a658ae
MusaviTFT <themusavi@gmail.com>
2022-02-01 03:02:13 +0100

    v2.1

--- cropperv2.py
+++ cropperv2.py
@@ -1,45 +1,49 @@
 from tkinter import filedialog
 from unittest import skip
-import os
 
+data = [("Text Files(*.txt)", "*.txt")]
 tf = filedialog.askopenfilename(
     initialdir="C:", title="Open Text file", filetypes=(("Text Files", "*.txt"),)
 )
 
-file1 = open(tf, "r", encoding="utf8")
-Lines = file1.readlines()
+mainFile = open(tf, "r", encoding="utf8")
+Lines = mainFile.readlines()
 cleanup = input("Clean up file? Y/N -")
 # cleanupStr = ['flags:','target_id:','object_id:','action_id:','skill_instance_id:','Instant/Triggered actionwas serialized to the client','Client couldn\'t execute','Connecting to instance server','Connect time to instance','Just before calling client','Got Instance Details from login server','Matching object found for','[Hellscape] ExitHellscape','Doodad hash:','Tile hash:']
-cleanupStr = ["@from", "@to", "died", "sirus", "maven",'elder', "%", "trade", " : "]
+cleanupStr = ["@from", "@to", "died", "sirus", "maven", "elder", "%", "trade", " : "]
 if cleanup.upper() == "Y":
-    file3 = open(
-        os.environ["USERPROFILE"] + "\Desktop\cleanedUp.txt", "w", encoding="utf8"
+    fileSaveClean = filedialog.asksaveasfilename(
+        defaultextension=data,
+        filetypes=data,
+        initialdir="C:",
+        title="Choose a save location of file",
     )
-    for line in Lines:
-        match = 0
-        if any(ele in line for ele in cleanupStr):
-            file3.writelines(line)
-        else:
-            skip
-    #     for cuStr in cleanupStr:
-    #         if cuStr.upper() in line.upper():
-    #             match = 1
-    #     if match == 0 : file3.writelines(line)
-    file3.close()
-    exit()
+    with open(fileSaveClean, "w", encoding="utf8") as fileClean:
+        for line in Lines:
+            match = 0
+            if any(ele in line for ele in cleanupStr):
+                fileClean.writelines(line)
+            else:
+                skip
+        fileClean.close()
+        exit()
 
 checkStr = input("Search query (separated with , if multiple queries):")
 splitStr = checkStr.split(",")
 
-file2 = open(os.environ["USERPROFILE"] + "\Desktop\cropped.txt", "w", encoding="utf8")
+fileSaveCrop = filedialog.asksaveasfilename(
+    defaultextension=data,
+    filetypes=data,
+    initialdir="C:",
+    title="Choose a save location of file",
+)
+with open(fileSaveCrop, "w", encoding="utf8") as fileCrop:
 
+    for line in Lines:
+        if any(ele in line for ele in splitStr):
+            fileSaveCrop.writelines(line)
+        else:
+            skip
 
-# Strips the newline character
-for line in Lines:
-    if any(ele in line for ele in splitStr):
-            file2.writelines(line)
-    else:
-        skip
-
-file2.close()
-file1.close()
+fileCrop.close()
+mainFile.close()
